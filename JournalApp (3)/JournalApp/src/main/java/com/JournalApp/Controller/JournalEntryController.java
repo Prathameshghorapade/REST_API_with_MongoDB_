ab33bad350e6593725440a1efc9d0cfe7caf273c
Prathameshghorapade <pghorapade17@gmail.com>
2025-06-02 21:33:37 +0530

    Add HTTP Status Codes

--- JournalApp (3)/JournalApp/src/main/java/com/JournalApp/Controller/JournalEntryController.java
+++ JournalApp (3)/JournalApp/src/main/java/com/JournalApp/Controller/JournalEntryController.java
@@ -4,10 +4,13 @@ import com.JournalApp.Entity.JournalEntry;
 import com.JournalApp.Service.JournalEntryService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -18,46 +21,64 @@ public class JournalEntryController {
  private JournalEntryService journalEntryService;
 
     @GetMapping("all-entries")
-    public List<JournalEntry>getAllEntries(){
+    public ResponseEntity<?>getAllEntries(){
 
-        return journalEntryService.getAllEntry();
+        List<JournalEntry> all=  journalEntryService.getAllEntry();
+        if (all != null && !all.isEmpty()){
+            return new ResponseEntity<>(all ,HttpStatus.OK);
+        }
+        return new ResponseEntity<>( HttpStatus.NOT_FOUND);
     }
 
 
     @PostMapping
-    public boolean addEntry(@RequestBody JournalEntry myentry){
+    public ResponseEntity<?> addEntry(@RequestBody JournalEntry myentry){
 
-        journalEntryService.saveEntry(myentry);
+       try {
+           journalEntryService.saveEntry(myentry);
 
-        return true;
+           return new ResponseEntity<>(myentry,HttpStatus.CREATED);
+       } catch (Exception e) {
+           return new ResponseEntity<>(myentry,HttpStatus.BAD_REQUEST);
+       }
     }
 
 
     @GetMapping("id/{myid}")
-    public JournalEntry getEntryById(@PathVariable ObjectId myid){
+    public ResponseEntity<?> getEntryById(@PathVariable ObjectId myid){
 
-       return journalEntryService.getById(myid).orElse(null);
+       Optional<JournalEntry> entry = journalEntryService.getById(myid);
+       if (entry.isPresent()){
+           return new ResponseEntity<>(entry.get(), HttpStatus.OK);
+       }
+        return new ResponseEntity<>( HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("delete/id/{id}")
-    public void deleteEntry(@PathVariable ObjectId id){
+    public ResponseEntity<?> deleteEntry(@PathVariable ObjectId id){
          journalEntryService.deleteEntryById(id);
 
+         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
       @PutMapping("id/{id}")
-    public JournalEntry updateEntry(@PathVariable ObjectId id,@RequestBody JournalEntry newEntry){
+    public ResponseEntity<JournalEntry> updateEntry(@PathVariable ObjectId id,@RequestBody JournalEntry newEntry){
 
         JournalEntry old =  journalEntryService.getById(id).orElse(null);
         if(old != null){
             old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals(" ") ? newEntry.getTitle() : old.getTitle());
             old.setContent(newEntry.getContent() != null && !newEntry.getContent().equals("") ? newEntry.getContent() : old.getContent());
+
+            journalEntryService.saveEntry(old);
+
+            return new  ResponseEntity<>(old,HttpStatus.OK);
+
         }
+          return new ResponseEntity<>( HttpStatus.NOT_FOUND);
 
-        journalEntryService.saveEntry(old);
 
-        return old;
-    }
+
+      }
 
 }
--- JournalApp (3)/JournalApp/src/main/java/com/JournalApp/Entity/JournalEntry.java
+++ JournalApp (3)/JournalApp/src/main/java/com/JournalApp/Entity/JournalEntry.java
@@ -5,39 +5,29 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
-import java.util.Date;
+
+
 
 @Document(collection = "Journal_Entries")
 public class JournalEntry {
 
 
     @Id
-    private ObjectId id;
+    private String id;
 
     private String title;
 
     private String content;
 
 
- private LocalDateTime date;
 
 
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public LocalDateTime setDate(LocalDateTime date) {
-       return this.date = date;
-
-    }
-
-    public ObjectId getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(ObjectId id) {
+    public void setId(String id) {
         this.id = id;
     }
 
